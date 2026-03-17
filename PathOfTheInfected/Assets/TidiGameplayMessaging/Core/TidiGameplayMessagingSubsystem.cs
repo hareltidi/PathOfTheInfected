5dfae02ae64551bfa325e39a33ae04d285ef17ae
harel tidi <harelt1904@gmail.com>
2026-03-17 13:03:48 +0200

    Docs
    Signed-off-by: harel tidi <harelt1904@gmail.com>

--- PathOfTheInfected/Assets/TidiGameplayMessaging/Core/TidiGameplayMessagingSubsystem.cs
+++ PathOfTheInfected/Assets/TidiGameplayMessaging/Core/TidiGameplayMessagingSubsystem.cs
@@ -20,8 +20,33 @@ namespace TidiGameplayMessaging.Core
         {
         }
 
+        /// <summary>
+        /// Gets the singleton instance of the gameplay messaging subsystem.
+        /// The instance is created lazily on first access.
+        /// </summary>
         public static TidiGameplayMessagingSubsystem Instance => _instance.Value;
 
+
+        /// <summary>
+        /// Subscribes a callback to a typed message channel.
+        /// </summary>
+        /// <typeparam name="TChannel">
+        /// The channel type used to route this message.
+        /// Must inherit from <see cref="TidiMessageChannel{TPayload}"/>.
+        /// </typeparam>
+        /// <typeparam name="TPayload">
+        /// The payload value type delivered to subscribers.
+        /// Must be a <c>struct</c> implementing <see cref="ITidiGameplayPayload"/>.
+        /// </typeparam>
+        /// <param name="callback">
+        /// The callback invoked when a message is published on <typeparamref name="TChannel"/>.
+        /// </param>
+        /// <returns>
+        /// An <see cref="IDisposable"/> token. Dispose it to unsubscribe the callback.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="callback"/> is <see langword="null"/>.
+        /// </exception>
         public IDisposable Subscribe<TChannel, TPayload>(Action<TPayload> callback)
             where TChannel : TidiMessageChannel<TPayload>
             where TPayload : struct, ITidiGameplayPayload
@@ -53,6 +78,19 @@ namespace TidiGameplayMessaging.Core
             });
         }
 
+        /// <summary>
+        /// Publishes a payload to the specified channel.
+        /// Messages are enqueued and drained in order to support safe re-entrant publishing.
+        /// </summary>
+        /// <typeparam name="TChannel">
+        /// The channel type that identifies the message route.
+        /// </typeparam>
+        /// <typeparam name="TPayload">
+        /// The payload value type carried with the message.
+        /// </typeparam>
+        /// <param name="payload">
+        /// The payload data sent to all current subscribers of the channel.
+        /// </param>
         public void Publish<TChannel, TPayload>(TPayload payload)
             where TChannel : TidiMessageChannel<TPayload>
             where TPayload : struct, ITidiGameplayPayload
@@ -67,6 +105,10 @@ namespace TidiGameplayMessaging.Core
             }
         }
 
+        /// <summary>
+        /// Drains the pending message queue and dispatches each message to subscribers.
+        /// Uses a listener snapshot to avoid collection-modified exceptions during callbacks.
+        /// </summary>
         private void DrainQueue()
         {
             _isDrainingQueue = true;
@@ -101,11 +143,19 @@ namespace TidiGameplayMessaging.Core
             }
         }
 
+        /// <summary>
+        /// Represents a queued message entry containing the destination channel and payload.
+        /// </summary>
         private readonly struct QueuedMessage
         {
             public readonly Type ChannelType;
             public readonly object Payload;
 
+            /// <summary>
+            /// Creates a queued message entry.
+            /// <param name="channelType">The message channel type to dispatch to.</param>
+            /// <param name="payload">The boxed payload instance.</param>
+            ///</summary>
             public QueuedMessage(Type channelType, object payload)
             {
                 ChannelType = channelType;
