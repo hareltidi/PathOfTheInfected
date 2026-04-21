cf82a95061aa8d9f5b5fb6942e2f885dc7349383
harel tidi <harelt1904@gmail.com>
2026-04-21 19:56:22 +0300

    some work

---
+++ PathOfTheInfected/Assets/PathOfTheInfected - Core/Scripts/Combat/Subsystems/Perks/PerksData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathOfTheInfected.Combat
+{
+    public abstract class PerkData : ScriptableObject
+    {
+        public string perkName;
+        public Sprite icon;
+
+        public List<PerkTier> tiers;
+    }
+
+    [Serializable]
+    public struct PerkTier
+    {
+        public int cost;
+        public float cooldown;
+        public float duration;
+
+        // generic "strength" value
+        public float magnitude;
+    }
+}
